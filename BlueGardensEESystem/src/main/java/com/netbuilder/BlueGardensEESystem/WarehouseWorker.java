6f39826a71509e6a7180d445b68a495f2919f858
lczornyj <lczornyj@PC-CZC046BG16.netbuilder.private>
2015-07-07 16:26:50 +0100

    attempted test for Supplier

--- BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/WarehouseWorker.java
+++ BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/WarehouseWorker.java
@@ -1,24 +1,24 @@
 package com.netbuilder.BlueGardensEESystem;
 
 public class WarehouseWorker {
-private int workerId;
-private String name;
-private String address;
 
+	private int workerId;
+	private String name;
+	private String address;
 
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getAddress() {
-	return address;
-}
-public void setAddress(String address) {
-	this.address = address;
-}
-public int getWorkerId() {
-	return workerId;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getWorkerId() {
+		return workerId;
 	}
 }
