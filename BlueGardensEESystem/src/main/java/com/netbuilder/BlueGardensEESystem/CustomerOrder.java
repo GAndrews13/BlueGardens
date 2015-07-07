8b101b2f05468d3d8f74bd64a76d522b6a6e7b2f
unknown <jmander@PC-CZC1198Q4Y.netbuilder.private>
2015-07-07 15:50:01 +0100

    Customer Order class added

---
+++ BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/CustomerOrder.java
@@ -0,0 +1,52 @@
+package com.netbuilder.BlueGardensEESystem;
+
+public class CustomerOrder {
+	
+	private int customerOrderID;
+	private boolean isAssigned;
+	private int trackingID;
+	private String deliveryType;
+	private int customerID;
+	private int productOrderLineID;
+	private int workerID;
+	
+	public int getCustomerOrderID() {
+		return customerOrderID;
+	}
+
+	
+	public boolean getIsAssigned() {
+		return isAssigned;
+	}
+	public void setIsAssigned(boolean isAssigned) {
+		this.isAssigned = isAssigned;
+	}
+	
+	
+	public int getTrackingID() {
+		return trackingID;
+	}
+
+	
+	public String getDeliveryType() {
+		return deliveryType;
+	}
+	public void setDeliveryType(String deliveryType) {
+		this.deliveryType = deliveryType;
+	}
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+	
+	public int getProductOrderLineID() {
+		return productOrderLineID;
+	}
+	
+	
+	public int getWorkerID() {
+		return workerID;
+	}
+
+
+}
