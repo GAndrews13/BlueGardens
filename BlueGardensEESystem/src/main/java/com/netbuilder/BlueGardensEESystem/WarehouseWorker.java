0cc7625e1820eaf8f857b4536dc808f7182450fe
lczornyj <lczornyj@PC-CZC046BG16.netbuilder.private>
2015-07-07 15:47:07 +0100

    supplier and Worker

---
+++ BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/WarehouseWorker.java
@@ -0,0 +1,24 @@
+package com.netbuilder.BlueGardensEESystem;
+
+public class WarehouseWorker {
+private int workerId;
+private String name;
+private String address;
+
+
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public int getWorkerId() {
+	return workerId;
+	}
+}
