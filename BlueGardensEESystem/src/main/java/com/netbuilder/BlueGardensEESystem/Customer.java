a8458c883352ef2c76596f5fce5ab05ab9dd2574
teksta7 <thomp194@uni.coventry.ac.uk>
2015-07-07 16:38:58 +0100

    Customer and Product Order Line entities created

---
+++ BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/Customer.java
@@ -0,0 +1,87 @@
+package com.netbuilder.test;
+
+public class Customer {
+	/**
+	 * @author Jake
+	 *	Attributes for customer including
+	 *	unique ID for customer (int)
+	 *	name, address, email and accountSTATUS are all Strings
+	 *	contactNUM is string so that String operations can be applied
+	 *	
+	 */
+	private int customerID;
+	private String name;
+	private String address;
+	private String email;
+	private String contactNUM;
+	private String accountSTATUS;
+	
+	//Put new customer info into database
+	private void Register()
+	{
+		
+	}
+	/*
+	private bool CheckRegistration(int customer_id)
+	{	
+		//return true if customer is registered else false
+		return false; 
+	}*/
+	/**
+	 * @author Jake
+	 *	Getters and Setter for class variables
+	 *	
+	 */
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContactNUM() {
+		return contactNUM;
+	}
+
+	public void setContactNUM(String contactNUM) {
+		this.contactNUM = contactNUM;
+	}
+
+	public String getAccountSTATUS() {
+		return accountSTATUS;
+	}
+
+	public void setAccountSTATUS(String accountSTATUS) {
+		this.accountSTATUS = accountSTATUS;
+	}
+	
+	
+	
+	
+
+}
---
+++ BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/ProductOrderLine.java
@@ -0,0 +1,44 @@
+package com.netbuilder.test;
+/**
+ * @author Jake
+ *
+ */
+public class ProductOrderLine 
+{	
+	/**
+	 * @author Jake
+	 * Attributes of the Product Order Line
+	 * including unique identifier and the quantity of the products 
+	 */
+	private int productOrderLineID;
+	private int productID;
+	private int quantity;
+	
+	
+	
+	/**
+	 * @author Jake
+	 *	Getters and Setters for class variables
+	 *	Initialise class
+	 */
+	public int getProductOrderLineID() {
+		return productOrderLineID;
+	}
+	public void setProductOrderLineID(int productOrderLineID) {
+		this.productOrderLineID = productOrderLineID;
+	}
+	public int getProductID() {
+		return productID;
+	}
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
