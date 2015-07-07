97678adce9a803b6a6e6475528ccb987f74e3402
GAndrews <gareth.andrews@netbuilder.com>
2015-07-07 14:59:55 +0100

    Added Maven Project

---
+++ BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/App.java
@@ -0,0 +1,13 @@
+package com.netbuilder.BlueGardensEESystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
