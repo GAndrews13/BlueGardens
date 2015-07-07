f66d7550f9b00234d7644142469aca2ae4d5219b
lczornyj <lczornyj@PC-CZC046BG16.netbuilder.private>
2015-07-07 16:44:07 +0100

    added commenting

--- BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/SupplierTest.java
+++ BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/SupplierTest.java
@@ -1,22 +1,29 @@
 package com.netbuilder.BlueGardensEESystem;
-
+/**
+ * @author lczornyj
+ * Attempting to do some testing for the supplier
+ * havent got a clue what im doing, just hashing it together
+ */
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class SupplierTest {
 
+	/**
+	 * @author lczornyj
+	 * I think this is testing if the supplier name is null
+	 */
 	@Test public void testSupplierName(){
 		Supplier testing = new Supplier();
 		assertNotNull(testing.getName());
 	}
-	@Test public void testSupplierGetName(){
+	/**
+	 * @author lczornyj
+	 * I think this one tests if the address is null
+	 */
+	@Test public void testSupplierGetAddress(){
 		Supplier testing = new Supplier();
-		assertEquals("a", testing.getName());
-	}
-	@Test public void testSupplierSetName(){
-		Supplier testing = new Supplier();
-		testing.setName("a");
-		assertEquals("a",testing.getName());
+		assertNotNull(testing.getAddress());
 	}
 }
