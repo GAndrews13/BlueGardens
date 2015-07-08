af44c90fe696589dc3f98376cc28ca46d0fca175
dogbonnahNB <david.ogbonnah@netbuilder.com>
2015-07-08 10:29:14 +0100

    Added Purchase Order

---
+++ BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/PurchaseOrder.java
@@ -0,0 +1,63 @@
+package com.netbuilder.BlueGardensEESystem;
+
+import java.util.ArrayList;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "nbgblue")
+
+/**
+ * @author David Ogbonnah
+ **/
+public class PurchaseOrder {
+
+	
+	private ArrayList<ProductOrderLine> pol;
+	public Supplier supp;
+	
+	public PurchaseOrder()
+	{
+		pol = new ArrayList<ProductOrderLine>();
+		supp = new Supplier();
+	}
+	
+	public void addProductOrderLine(ProductOrderLine p)
+	{
+		pol.add(p);
+	}
+	
+	public ArrayList<ProductOrderLine> getProductOrderLines()
+	{
+		return pol;
+	}
+	public ProductOrderLine getProductOrderLine(ProductOrderLine p)
+	{
+		int i = 0;
+	    ProductOrderLine polInstance = null;
+	    while (i < pol.size())
+	    {
+	    	polInstance = pol.get(i);
+	    	if(polInstance.equals(p))
+	    	{
+	    		return p;
+	    	}
+	    	
+	    	i++;
+	    }
+	    
+	    return null;
+	}
+	
+	public void Supplier(Supplier s)
+	{
+		supp = s;
+	}
+	
+	public Supplier getSupplier()
+	{
+		return supp;
+	}
+	
+}
