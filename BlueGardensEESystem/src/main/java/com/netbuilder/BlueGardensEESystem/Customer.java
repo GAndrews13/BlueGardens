6dadd1c9f499c2a2301d8b4ce827d1d9e154deb5
teksta7 <thomp194@uni.coventry.ac.uk>
2015-07-08 10:44:12 +0100

    Customer and Product Order line changed to entities

--- BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/Customer.java
+++ BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/Customer.java
@@ -1,5 +1,17 @@
 package com.netbuilder.BlueGardensEESystem;
 
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;;
+
+@Entity
+@Table(name = "Customer")
+
 public class Customer {
 	/**
 	 * @author Jake
@@ -9,13 +21,66 @@ public class Customer {
 	 *	contactNUM is string so that String operations can be applied
 	 *	
 	 */
+	
+	/**
+	 * Customer ID will be the primary key in the customer table/ NOT NULL 
+	 */
+	@Id
+	@Column(name = "customerID", nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
 	private int customerID;
+	
+	/**
+	 * Name represents the customer name in the table, cannot be null
+	 */
+	@Column(name = "Name", nullable = false, length = 127)
+	@NotNull
+	@Size(min = 2, max = 127)
 	private String name;
+	
+	/**
+	 * Address represents the customer address, length may need to be longer
+	 */
+	@Column(name = "Address", nullable = false, length = 45)
+	@NotNull
+	@Size(min = 5, max = 45)
 	private String address;
+	
+	/**
+	 * Email can be an optional field to enter, i.e can be null
+	 */
+	@Column(name = "Email", nullable = true, length = 45)
+	@Size(min = 4, max = 45)
 	private String email;
+	
+	/**
+	 * Contact number for the customer, cannot be null 
+	 */
+	@Column(name = "Contact Number", nullable = false, length = 45)
+	@NotNull
+	@Size(min = 10, max = 45)
 	private String contactNUM;
+	
+	/**
+	 * Account status cannot be null but can have a value like N/A for not active 
+	 */
+	@Column(name = "Account Status", nullable = false, length = 45)
+	@NotNull
+	@Size(min = 2, max = 45)
 	private String accountSTATUS;
 	
+	/**
+	 * Initialize customer so instances of customer cannot have null values 
+	 */
+	public Customer(int customerID, String name, String address, String contactNUM, String accountSTATUS)
+	{
+		this.customerID = customerID;
+		this.name = name;
+		this.address = address;
+		this.contactNUM = contactNUM;
+		this.accountSTATUS = accountSTATUS;
+	}
 	//Put new customer info into database
 	private void Register()
 	{
