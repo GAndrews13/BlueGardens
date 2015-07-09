6a65dca25b39317ea526b2483521a24a61e44f07
unknown <abalagel@PC-CZC4165059.netbuilder.private>
2015-07-09 15:17:41 +0100

    db

--- BlueGardensEESystem/src/main/java/com/netbuilder/entities/Basket.java
+++ BlueGardensEESystem/src/main/java/com/netbuilder/entities/Basket.java
@@ -1,18 +1,24 @@
 package com.netbuilder.entities;
 
 import java.util.ArrayList;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table
+@NamedQueries({ 
+	@NamedQuery(name = Basket.FIND_OUT_BY_PRODUCTID, query = "SELECT b FROM Basket WHERE b.productId = 1")	
+})
 public class Basket {
 /**
  * @author abalagel
@@ -20,6 +26,7 @@ public class Basket {
  * unique customer id from customer table
  * list of order lines that each contains a product and its required quantity passed from ProductOrderLine.java
 */	
+	public static final String FIND_OUT_BY_PRODUCTID = "Basket.findByProductId";
 	@Id
 	@Column (name = "Customer ID", nullable = false)
 	@NotNull
