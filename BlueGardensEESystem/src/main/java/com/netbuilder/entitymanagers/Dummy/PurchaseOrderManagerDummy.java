489734470c65469cb9463240a47d9f4a62cade20
dogbonnahNB <david.ogbonnah@netbuilder.com>
2015-07-09 14:48:51 +0100

    Moved POMD class

---
+++ BlueGardensEESystem/src/main/java/com/netbuilder/entitymanagers/Dummy/PurchaseOrderManagerDummy.java
@@ -0,0 +1,62 @@
+package com.netbuilder.entitymanagers.Dummy;
+
+import java.util.ArrayList;
+
+import com.netbuilder.BlueGardensEESystem.PersistenceManager;
+import com.netbuilder.entities.DeliveryLocation;
+import com.netbuilder.entities.PurchaseOrder;
+import com.netbuilder.entities.Supplier;
+import com.netbuilder.entitymanagers.PurchaseOrderManager;
+
+public class PurchaseOrderManagerDummy implements PurchaseOrderManager 
+{
+	private ArrayList<PurchaseOrder> pol;
+	private PersistenceManager pm;
+	
+	public void persistPurchaseOrder(PurchaseOrder po)
+	{
+		pol.add(po);
+	}
+	public void persistPurchaseOrders(ArrayList<PurchaseOrder> purchaseOrders)
+	{
+		pol.addAll(purchaseOrders);
+	}
+	
+	public void updatePurchaseOrder(PurchaseOrder po)
+	{
+		for(PurchaseOrder po1: pol)
+		{
+			pol.set(pol.indexOf(po1), po);
+		}
+		
+	}
+	
+	public PurchaseOrder findByDeliveryLocation(DeliveryLocation dl)
+	{
+		PurchaseOrder poInst = null;
+		for(PurchaseOrder po1: pol)
+		{
+			if(po1.getDeliveryLocation() == dl) poInst = po1;
+		}
+		return poInst;
+	}
+	public PurchaseOrder findByOutSupplier(Supplier supp)
+	{
+		PurchaseOrder poInst = null;
+		for(PurchaseOrder po1: pol)
+		{
+			if(po1.getSupplier().equals(supp)) poInst = po1;
+		}
+		return poInst;
+	}
+	public PurchaseOrder findById(int id)
+	{
+		PurchaseOrder poInst = null;
+		for(PurchaseOrder po1: pol)
+		{
+			if(po1.getID() == id) poInst = po1;
+		}
+		return poInst;
+	}
+
+}
