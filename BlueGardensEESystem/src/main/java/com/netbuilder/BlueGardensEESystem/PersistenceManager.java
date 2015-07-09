11e3d1bd7df39b4a588bc40643481cecfbd83648
dogbonnahNB <david.ogbonnah@netbuilder.com>
2015-07-09 10:13:28 +0100

    PersistenceManager Created

---
+++ BlueGardensEESystem/src/main/java/com/netbuilder/BlueGardensEESystem/PersistenceManager.java
@@ -0,0 +1,32 @@
+package com.netbuilder.BlueGardensEESystem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+
+public class PersistenceManager 
+{
+	 
+	private EntityManagerFactory emf;
+	public EntityManager CreateEntityManager()
+	{
+		try
+		{
+			emf = Persistence.createEntityManagerFactory("BlueGardensFactory");
+			EntityManager em = emf.createEntityManager();
+			return em;
+		} 
+		catch (PersistenceException pe)
+		{
+			return null;
+		}
+	}
+	
+	public void CloseEntityManager(EntityManager em)
+	{
+		em.close();
+		emf.close();
+	}
+	
+}
