0230e4978c68d6c868a1e506264d5c78a3327a88
GAndrews <gareth.andrews@netbuilder.com>
2015-07-20 17:35:21 +0100

    Paypal button intergrated

--- BlueGardensEESystem/src/main/java/com/netbuilder/util/UserDetails.java
+++ BlueGardensEESystem/src/main/java/com/netbuilder/util/UserDetails.java
@@ -6,11 +6,29 @@ import javax.inject.Named;
 @Named
 @RequestScoped
 public class UserDetails {
-	public String username;
-	public String password;
+	private String username;
+	private String password;
 	
 	public UserDetails(String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
