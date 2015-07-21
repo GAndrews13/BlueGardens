d456ba602511f8d78b5acacff9d45974d9a5fd0e
lczornyj <lczornyj@PC-CZC046BG16.netbuilder.private>
2015-07-21 13:53:50 +0100

    changed product controller to include validation and documentation

--- BlueGardensEESystem/src/main/java/com/netbuilder/controllers/ProductController.java
+++ BlueGardensEESystem/src/main/java/com/netbuilder/controllers/ProductController.java
@@ -1,29 +1,39 @@
 package com.netbuilder.controllers;
 
+/**
+ * Created Controller for the specific product page. The products are found via the products Image link,
+ * therefor all products need to have an image or a location for an image in order to follow through to
+ * the specific product page. There is also some validation for if the product id does not exist.
+ * @author lczornyj
+ */
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.junit.runner.Request;
-
 import com.netbuilder.entities.Product;
 import com.netbuilder.entitymanagers.ProductManager;
-
 
 @Named
 @RequestScoped
 public class ProductController {
-	
+
 	@Inject
 	private ProductManager productmanager;
-	
+
 	public Product product;
-	
-	//find product details by the findById(id) where the id is the specific product id.
-	
-	private String id =product.getImageLink().getPathInfo();
-	
+
+	// find product details by the findById(id) where the id is the specific
+	// product id.
+
+	private String id = product.getImageLink();
+
 	public ProductController() {
-		product = productmanager.findById(id);	
+		id = id.replace("www.NBGardens.com/Products/", "");
+		try {
+			product = productmanager.findById(Integer.parseInt(id));
+		} catch (Exception e) {
+			String errormsg = "please enter details";
+
+		}
 	}
 }
