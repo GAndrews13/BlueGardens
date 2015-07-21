281fc9e6c067892c92bcfd489e39d483a2a580d8
GAndrews <gareth.andrews@netbuilder.com>
2015-07-21 13:35:40 +0100

    Added paypal interaction to template

---
+++ BlueGardensEESystem/src/main/java/com/netbuilder/controllers/CheckoutController.java
@@ -0,0 +1,29 @@
+package com.netbuilder.controllers;
+
+public class CheckoutController {
+	/*
+	 * Error message that can be returned to the users
+	 */
+	private String errormsg;
+	public String getErrorMessage()
+	{
+		return errormsg;
+	}
+	public void setErrorMessage(String inErrorMessage)
+	{
+		errormsg = inErrorMessage;
+	}
+	
+	
+	public void generateProductList()
+	{
+		
+	}
+	
+	public void sendToPaypal()
+	{
+		
+	}
+	
+	
+}
