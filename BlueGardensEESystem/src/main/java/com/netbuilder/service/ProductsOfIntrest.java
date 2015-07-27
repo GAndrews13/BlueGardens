72dc1bce127c1d585c40b50da453032c49dbc9d3
lczornyj <lczornyj@PC-CZC046BG16.netbuilder.private>
2015-07-27 16:59:15 +0100

    logic attempt and going nuts doing it

--- BlueGardensEESystem/src/main/java/com/netbuilder/service/ProductsOfIntrest.java
+++ BlueGardensEESystem/src/main/java/com/netbuilder/service/ProductsOfIntrest.java
@@ -7,6 +7,7 @@ package com.netbuilder.service;
  * 
  */
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -26,6 +27,12 @@ public class ProductsOfIntrest {
 	private ArrayList<Product> trendingList = new ArrayList<Product>();
 	private ArrayList<Product> pourouswareList = new ArrayList<Product>();
 	private Product product;
+	private ArrayList<ArrayList<Product>> list = new ArrayList<ArrayList<Product>>();
+	private Random generator = new Random();
+	
+	private int sale;
+	private int pw;
+	private int trending;
 	
 	public void  createLists(){
 		for (int i = 0; i < products.size(); i++){
@@ -51,49 +58,102 @@ public class ProductsOfIntrest {
 	 * @return returns an Array list of array lists of products.
 	 */
 	public ArrayList<ArrayList<Product>> listSizes(){
-		ArrayList<ArrayList<Product>> list = new ArrayList<ArrayList<Product>>();
 		if (saleList.size() <= pourouswareList.size()) {
 			if(saleList.size() <= trendingList.size()) {
 				list.set(0, saleList);
+				sale = 0;
 				if (pourouswareList.size() <= trendingList.size()) {
 					list.set(1, pourouswareList);
+					pw = 1;
 					list.set(2, trendingList);
+					trending = 2;
 				} else {
 					list.set(1, trendingList);
+					trending = 1;
 					list.set(2, pourouswareList);
+					pw = 2;
 				}
 			} else {
 				list.set(0, trendingList);
+				trending = 0;
 				list.set(1, saleList);
+				sale = 1;
 				list.set(2, pourouswareList);
+				pw = 2;
 			}
 		} else { 
 			if (pourouswareList.size() <= trendingList.size()) {
 				list.set(0, pourouswareList);
+				pw = 0;
 				if(saleList.size() <= trendingList.size()) {
 					list.set(1, saleList);
+					sale = 1;
 					list.set(2, trendingList);
+					trending = 2;
 				} else {
 					list.set(1, trendingList);
+					trending = 1;
 					list.set(2, saleList);
+					sale = 2;
 				}
 			} else {
 				list.set(0, trendingList);
+				trending = 0;
 				list.set(1, pourouswareList);
+				pw = 1;
 				list.set(2, saleList);
+				sale = 2;
 			}
 		}
 		return list;
 	}
 	
-	public ArrayList<Product> getFourSale() {
-		// TODO Auto-generated method stub
-		return null;
+	public void deleteDuplicate(ArrayList<Product> chosenList, int category) {
+		
+		
+		
+		
+		
+		int j = 0;
+			for (int i = 0; i < list.get(i).size(); i++) {
+				String currentname = list.get(j).get(i).getProductName();
+				
+						for(int a = 0; a < list.get(j+1).size(); a++) {
+							if(list.get(j+1).get(i).getProductName() == currentname)
+							{
+								list.get(j+1).remove(i);
+							}
+							if(list.get(j+2).get(i).getProductName() == currentname)
+							{
+								list.get(j+2).remove(i);
+							}
+						}
+					}
+			j=j+1;
+			for (int i = 0; i < list.get(i).size(); i++) {
+				String currentname = list.get(j).get(i).getProductName();
+				
+						for(int a = 0; a < list.get(j+1).size(); a++) {
+							if(list.get(j+1).get(i).getProductName() == currentname)
+							{
+			 					list.get(j+1).remove(i);
+							}
+					}
+			}
 	}
-
-	public ArrayList<Product> getThreeNew() {
-		// TODO Auto-generated method stub
-		return null;
+		
+	
+	public ArrayList<Product> getFourSale() {
+		ArrayList<Product> chosen = new ArrayList<Product>();
+		Product num1;
+		for(int i = 0; i < 3; i++) {
+			int randomProduct = generator.nextInt(list.get(sale).size());
+			num1 = list.get(sale).get(randomProduct);
+			
+			chosen.add(num1);
+		}
+		deleteDuplicate(chosen,1);
+		return chosen;
 	}
 
 	public ArrayList<Product> getThreeTrending() {
