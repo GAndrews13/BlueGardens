61fd341199238725e51706598235475b86f3740f
unknown <jmander@PC-CZC1198Q4Y.netbuilder.private>
2015-07-28 17:29:19 +0100

    jordan

--- BlueGardensEESystem/src/main/java/com/netbuilder/controllers/PasswordResetController.java
+++ BlueGardensEESystem/src/main/java/com/netbuilder/controllers/PasswordResetController.java
@@ -1,5 +1,44 @@
 package com.netbuilder.controllers;
 
-public class PasswordResetController {
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.inject.Named;
 
+import com.netbuilder.entities.CustomerLogin;
+import com.netbuilder.entitymanagers.CustomerLoginManager;
+import com.netbuilder.util.PasswordReset;
+
+/**
+ * @author jmander
+ **/
+
+@Named
+@Dependent
+public class PasswordResetController {
+	@Inject
+	private CustomerLoginManager customerLoginManager;
+	@Inject
+	private CustomerLogin customerLogin;
+	//@Inject
+	private PasswordReset passwordReset;
+	public String errormsg;
+	
+	public String getErrormsg() {
+		return errormsg;
+	}
+
+	public void setErrormsg(String errormsg) {
+		this.errormsg = errormsg;
+	}
+
+	public String passwordReset() {
+		if(passwordReset.getNewPassword() != passwordReset.getConfirmNewPassword()){
+			errormsg = "Passwords do not match";
+			return "passwordReset";
+		}
+		//customer login manager needs an update, to fire off the new password below
+		customerLogin.setCustomerPassword(passwordReset.getNewPassword());
+		//Fire off new password to customerLogin
+		return "login";
+	}
 }
