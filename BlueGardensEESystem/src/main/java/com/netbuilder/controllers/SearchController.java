c6cdb8e2e5baa3a5fd6eaff85bf33a1fae188213
James Thompson <james.thompson@netbuilder.com>
2015-08-03 13:23:04 +0100

    looking at the search controller

--- BlueGardensEESystem/src/main/java/com/netbuilder/controllers/SearchController.java
+++ BlueGardensEESystem/src/main/java/com/netbuilder/controllers/SearchController.java
@@ -2,39 +2,37 @@ package com.netbuilder.controllers;
 
 import java.util.ArrayList;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import com.netbuilder.entities.Product;
+import com.netbuilder.entitymanagers.ProductManager;
 import com.netbuilder.service.ProductsOfIntrest;
 /**
  *  Handles input from the page and passes it to the logic contained within products of intrest
  * @author Jake
  *
  */
+@Named
+@RequestScoped
 public class SearchController 
 {
 	//private long id;
 	//private String name;
 	//private boolean stock;
 	private ArrayList <Product> results;
-	private Product pResult;
-	private ProductsOfIntrest search;
-	public String errorMSG;
+	private String searchTerm;
+	@Inject
+	private ProductManager productManager;
 	
-	public SearchController(int ID, String NAME, boolean STOCK)
-	{
-		try
-		{
-		results = search.getSearchResults(ID, NAME, STOCK);
-		pResult = results.get(0);
-		}
-		catch(Exception e)
-		{
-			errorMSG = "Search failed please try again";
-		}
-	}
-
-	public Product getpResult() {
-		return pResult;
-	}
-
 	
+	public ArrayList<Product> getResults() { return results; }
+	public String getSearchTerm() { return searchTerm; }
+	public void setResults(ArrayList<Product> results) { this.results = results; }
+	public void setSearchTerm(String searchTerm) { this.searchTerm = searchTerm; }
+	
+	public void searchByTerm() {
+		results = productManager.findByName(searchTerm);
+	}
 }
