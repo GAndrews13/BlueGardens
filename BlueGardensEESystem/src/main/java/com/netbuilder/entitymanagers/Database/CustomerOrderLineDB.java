d31f5bf40e1505e1de76c21f79b569b45384cec8
teksta7 <thomp194@uni.coventry.ac.uk>
2015-08-04 13:08:51 +0100

    menu background images added

---
+++ BlueGardensEESystem/src/main/java/com/netbuilder/entitymanagers/Database/CustomerOrderLineDB.java
@@ -0,0 +1,81 @@
+package com.netbuilder.entitymanagers.Database;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+import com.netbuilder.BlueGardensEESystem.PersistenceManager;
+import com.netbuilder.entities.CustomerOrderLine;
+import com.netbuilder.entities.ProductOrderLine;
+import com.netbuilder.entitymanagers.CustomerOrderLineManager;
+
+public class CustomerOrderLineDB implements CustomerOrderLineManager 
+{
+	@Inject
+	private PersistenceManager pm;
+	
+	
+	@Override
+	public void persistCOL(CustomerOrderLine col)
+	{
+		EntityManager em = pm.CreateEntityManager();
+		em.getTransaction().begin();
+		em.persist(col);
+		em.getTransaction().commit();
+		pm.CloseEntityManager(em);
+	}
+
+	@Override
+	public ArrayList<CustomerOrderLine> findByPOID(int po_ID) {
+		EntityManager em = pm.CreateEntityManager(); 
+		ArrayList<CustomerOrderLine> col = (ArrayList<CustomerOrderLine>) em.createQuery("select * from ProductOrderLine a", CustomerOrderLine.class).getResultList(); 
+		pm.CloseEntityManager(em);
+		try
+		{
+			return col; 
+		}
+		catch (NoResultException nre) 
+			{
+				return null;
+			} 		
+	}
+
+	@Override
+	public CustomerOrderLine findByCOID(int customerID) 
+	{
+		EntityManager em = pm.CreateEntityManager(); 
+		TypedQuery<CustomerOrderLine> tq = em.createNamedQuery(CustomerOrderLine.FIND_BY_CO_ID, CustomerOrderLine.class);
+		pm.CloseEntityManager(em);
+		tq.setParameter("CustomerID", customerID);
+		try
+		{
+			return tq.getSingleResult(); 
+		}
+		catch (NoResultException nre) 
+			{
+				return null;
+			} 
+	}
+
+	@Override
+	public ArrayList<CustomerOrderLine> findByQuantity(int quantity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateCOL(CustomerOrderLine pol) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<CustomerOrderLine> findall() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
