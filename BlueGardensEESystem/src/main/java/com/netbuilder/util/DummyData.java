3cffd6d9b6011edcb46659f90f604436999f74fe
ancabalagel <anca.balagel@netbuilder.com>
2015-08-10 14:00:16 +0100

    images in slider

--- BlueGardensEESystem/src/main/java/com/netbuilder/util/DummyData.java
+++ BlueGardensEESystem/src/main/java/com/netbuilder/util/DummyData.java
@@ -132,20 +132,20 @@ public class DummyData {
 	}
 
 	public DummyData(){
-		product1 = new Product(1, "Ketchup", 10, 3, 10, false,
+		product1 = new Product(1, "Gnomes On Swing", 10, 3, 10, false,
 				135.00, false, "that's", "ammmaaaazing", "that", "you", "know",
-				"resources/img/ketchup.png", true);
+				"resources/img/gnomesswing.png", true);
 		
-		product2 = new Product(2, "Chocolate", 100, 30, 100, false,
+		product2 = new Product(2, "Mini Garden", 100, 30, 100, false,
 				10.00, false, "that's", "ammmaaaazing", "that", "you", "know",
-				"resources/img/chocolate.png", true);
+				"resources/img/minigarden.png", true);
 		
 		product3 = new Product(3, "Notts County Gnome", 70, 50, 180, false,
 				85.00, false, "that's", "ammmaaaazing", "that", "you", "know",
 				"resources/img/nottsCountyGnome.png", true);
 		
-		product4 = new Product(4, "Floor", 100, 70, 700, true,
-				98.00, false, "that's", "ammmaaaazing", "that", "you", "know",
+		product4 = new Product(4, "Deck", 100, 70, 700, true,
+				98.00, false, "Wooden Deck", "ammmaaaazing", "that", "you", "know",
 				"resources/img/floor.png", false);
 		
 		product5 = new Product(5, "Machine Gun Gnome", 120, 60, 140, true,
@@ -156,13 +156,13 @@ public class DummyData {
 				4.99, false, "that's", "ammmaaaazing", "that", "you", "know",
 				"resources/img/plum.png", false);
 		
-		product7 = new Product(7, "Cheese", 150, 13, 163, false,
+		product7 = new Product(7, "Lighting", 150, 13, 163, false,
 				12.00, true, "that's", "ammmaaaazing", "that", "you", "know",
-				"resources/img/cheese.png", false);
+				"resources/img/light.png", false);
 		
-		product8 = new Product(8, "Sofa", 140, 36, 164, false,
+		product8 = new Product(8, " Garden Bench", 140, 36, 164, false,
 				700.00, true, "that's", "ammmaaaazing", "that", "you", "know",
-				"resources/img/sofa.png", false);
+				"resources/img/bench.png", false);
 		
 		product9 = new Product(9, "Jacuzzi", 120, 34, 150, false,
 				15.00, true, "that's", "ammmaaaazing", "that", "you", "know",
