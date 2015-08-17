656bebbf75fcad564e8531347bb08355d056b356
JCMander <Jordan.Mander@netbuilder.com>
2015-08-17 11:46:29 +0100

    dummy data updated

--- CallCentre/src/main/java/com/netbuilder/model/Utility/DummyData.java
+++ CallCentre/src/main/java/com/netbuilder/model/Utility/DummyData.java
@@ -5,15 +5,13 @@ import java.util.ArrayList;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
-import com.netbuilder.BlueGardensEESystem.DeliveryStatus;
-import com.netbuilder.entities.Customer;
-import com.netbuilder.entities.CustomerLogin;
-import com.netbuilder.entities.CustomerOrder;
-import com.netbuilder.entities.CustomerOrderLine;
-import com.netbuilder.entities.Product;
-import com.netbuilder.entities.WarehouseLocation;
-import com.netbuilder.entities.WarehouseWorker;
-import com.netbuilder.entities.WishlistItems;
+import com.netbuilder.model.DeliveryStatus;
+import com.netbuilder.model.Customer;
+import com.netbuilder.model.CustomerLogin;
+import com.netbuilder.model.CustomerOrder;
+import com.netbuilder.model.CustomerOrderLine;
+import com.netbuilder.model.Product;
+import com.netbuilder.model.WarehouseWorker;
 
 @Singleton
 @Startup
@@ -36,22 +34,6 @@ public class DummyData {
 	private Product product8;
 	private Product product9;
 	private Product product10;
-	private WarehouseLocation location1;
-	private WarehouseLocation location2;
-	private WarehouseLocation location3;
-	private WarehouseLocation location4;
-	private WarehouseLocation location5;
-	private WarehouseLocation location6;
-	private WarehouseLocation location7;
-	private WarehouseLocation location8;
-	private WarehouseLocation location9;
-	private WarehouseLocation location10;
-	private WishlistItems wishlist1 = new WishlistItems(1, 1);
-	private WishlistItems wishlist2 = new WishlistItems(1, 4);
-	private WishlistItems wishlist3 = new WishlistItems(1, 7);
-	private WishlistItems wishlist4 = new WishlistItems(2, 2);
-	private WishlistItems wishlist5 = new WishlistItems(2, 4);
-	private WishlistItems wishlist6 = new WishlistItems(2, 8);
 	private CustomerOrder customerOrder1;
 	private CustomerOrder customerOrder2;
 	private CustomerOrder customerOrder3;
@@ -76,26 +58,12 @@ public class DummyData {
 	private CustomerOrderLine customerOrderLine15;
 	private CustomerOrderLine customerOrderLine16;
 	private CustomerOrderLine customerOrderLine17;
-	private ArrayList<WarehouseLocation> locations = new ArrayList<WarehouseLocation>();
 	private ArrayList<WarehouseWorker> workers = new ArrayList<WarehouseWorker>();
-	private ArrayList<WishlistItems> wishlistProducts = new ArrayList<WishlistItems>();
 	private ArrayList<CustomerOrderLine> customerOrderLines = new ArrayList<CustomerOrderLine>();
 	private ArrayList<CustomerOrder> customerOrders = new ArrayList<CustomerOrder>();
 	private ArrayList<Product> products = new ArrayList<Product>();
 	private ArrayList<Customer> customers = new ArrayList<Customer>();
 	private ArrayList<CustomerLogin> customerLogins = new ArrayList<CustomerLogin>();
-	
-	public ArrayList<WarehouseLocation> getLocations() {
-		return locations;
-	}
-
-	public void setLocation(WarehouseLocation location) {
-		locations.add(location);
-	}
-
-	public void setLocations(ArrayList<WarehouseLocation> locations) {
-		this.locations.addAll(locations);
-	}
 
 	public ArrayList<WarehouseWorker> getWorkers() {
 		return workers;
@@ -170,79 +138,54 @@ public class DummyData {
 		this.customerLogins.addAll(customerLogins);
 	}
 
-	public void setWishlistItems(WishlistItems wishlistitem) {
-		wishlistProducts.add(wishlistitem);
-	}
-
-	public ArrayList<WishlistItems> getWishlistProducts() {
-		return wishlistProducts;
-	}
-
-	public void setWishlistProducts(ArrayList<WishlistItems> wishlistProducts) {
-		this.wishlistProducts.addAll(wishlistProducts);
-	}
-
 	public DummyData() {
 		product1 = new Product(1, "Gnomes On Swing", 10, 3, 10, true, 134.99,
 				false, "height: 30 cm", "material: polyresin", "frost proof and weather resistant", "porousware", "handcrafted",
 				"resources/img/gnomesswing.png", false);
-		
-		location1 = new WarehouseLocation(1, "A1", 10);
 
 		product2 = new Product(2, "Mini Garden", 100, 30, 100, false, 19.99,
 				false, "height: 55 cm", "diameter: 30 cm ", "made to order", "handcrafted", "weight: 7.5 kg",
 				"resources/img/minigarden.png", true);
-		
-		location2 = new WarehouseLocation(2, "A2", 100);
 
 		product3 = new Product(3, "Arsenal Gnome", 70, 50, 180, true,
 				84.99, false, "height: 60 cm", "material: clay", "Arsenal Approved", "porousware", "water resistant",
 				"resources/img/nottsCountyGnome.png", false);
 		
-		location3 = new WarehouseLocation(3, "A3", 70);
 
 		product4 = new Product(4, "Deck", 100, 70, 700, false, 14.99, true,
 				"price per metre squared", "material: mahogany", "size: custom ", "water resistant", "non-slip",
 				"resources/img/floor.png", false);
 		
-		location4 = new WarehouseLocation(4, "C4", 100);
 
 		product5 = new Product(5, "Machine Gun Gnome", 120, 60, 140, true,
 				54.99, false, "height: 37.5 cm", "angry face gnome", "'Say Hello to my little friend!'", "porousware", "handcrafted",
 				"resources/img/gnome.png", false);
 		
-		location5 = new WarehouseLocation(5, "J8", 120);
 
 		product6 = new Product(6, "Garden Fountain", 104, 63, 130, false, 4499.99, false,
 				"suitable for outdoors", "material: granite", "size: 2 m", "diameter: 5 m ", "supplied with a pump, liner and hoses",
 				"resources/img/fountain.png", true);
 		
-		location6 = new WarehouseLocation(6, "F2", 104);
 
 		product7 = new Product(7, "Garden Lights", 150, 13, 163, false, 119.99,
 				true, "height: 55 cm ", "diameter: 30 cm", "solar powered", "color: custom", "plants included",
 				"resources/img/lights.png", false);
 		
-		location7 = new WarehouseLocation(7, "L9", 150);
 
 		product8 = new Product(8, "Garden Bench", 140, 36, 164, false, 199.99,
 				true, "height: 1 m", "width: 2 m", "material: chestnut", "water resistant", "hand crafted by local carpenters",
 				"resources/img/bench.png", false);
 		
-		location8 = new WarehouseLocation(8, "S3", 140);
 
 		product9 = new Product(9, "Jacuzzi", 120, 34, 150, false, 24999.99, false,
 				"24 carat gold plated", "20 water jets", "in-build heating", "glass holders", "customisable seating",
 				"resources/img/jacuzzi.png", true);
 		
-		location9 = new WarehouseLocation(9, "H7", 120);
 
 		product10 = new Product(10, "Heater", 230, 25, 300, true, 31.99, true,
 				"material: Tungston steel", "size: 75 cm", "color: metalic black", "rustic design", "suitable for outdoors", 
 				"resources/img/heater.png",
 				true);
-		
-		location10 = new WarehouseLocation(10, "K9", 230);
 
 		products.add(product1);
 		products.add(product2);
@@ -254,17 +197,6 @@ public class DummyData {
 		products.add(product8);
 		products.add(product9);
 		products.add(product10);
-		
-		locations.add(location1);
-		locations.add(location2);
-		locations.add(location3);
-		locations.add(location4);
-		locations.add(location5);
-		locations.add(location6);
-		locations.add(location7);
-		locations.add(location8);
-		locations.add(location9);
-		locations.add(location10);
 		
 		worker1 = new WarehouseWorker(1, "Tony Stark", "Malibu Beach House, Malibu, 1R0N M4N", false, false, "p3pp3r");
 		worker2 = new WarehouseWorker(2, "Steven Rogers", "SHIELD, Secret Location, CPT US4", false, false, "c4rt3r");
@@ -327,13 +259,6 @@ public class DummyData {
 				0, DeliveryStatus.ORDER_PLACED, (1079.92 + 1439.88 + 44999.90));
 		// new CustomerOrder(customerOrderID, isAssigned, trackingID,
 		// deliveryType, customerID, workerID, status, price)
-
-		wishlistProducts.add(wishlist1);
-		wishlistProducts.add(wishlist2);
-		wishlistProducts.add(wishlist3);
-		wishlistProducts.add(wishlist4);
-		wishlistProducts.add(wishlist5);
-		wishlistProducts.add(wishlist6);
 
 		customerOrders.add(customerOrder1);
 		customerOrders.add(customerOrder2);
