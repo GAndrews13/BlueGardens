10db3c629e4615173aa0d98dd7b2750c6a818c20
GAndrews <gareth.andrews@netbuilder.com>
2015-08-25 10:35:15 +0100

    Basket annotated with appropiate information

--- BlueGardensEESystem/src/main/java/com/netbuilder/entities/Basket.java
+++ BlueGardensEESystem/src/main/java/com/netbuilder/entities/Basket.java
@@ -1,18 +1,23 @@
 package com.netbuilder.entities;
 
 import java.util.ArrayList;
-
-public class Basket {
-	/**
-	 * @author abalagel Attributes for the Basket unique customer id from
+/**
+ * 
+ * @author @author abalagel Attributes for the Basket unique customer id from
 	 *         customer table list of order lines that each contains a product
 	 *         and its required quantity passed from CustomerOrderLine.java
-	 */
-
+ */
+public class Basket {
 	private long customerId;
 	private ArrayList<CustomerOrderLine> customerOrderLines;
 	private int customerOrderID;
 
+	/**
+	 * Uses a customer id, the order id and the products they wish to order
+	 * @param customerOrderID
+	 * @param customerId
+	 * @param customerOrderLines
+	 */
 	public Basket(int customerOrderID, long customerId, ArrayList<CustomerOrderLine> customerOrderLines) {
 		this.customerOrderID = customerOrderID;
 		this.customerId = customerId;
